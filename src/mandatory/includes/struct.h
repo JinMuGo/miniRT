ae46a70bbac60beb86ada82dc45ea55732a1cbf3
Sanghwa Lee <sanghwal@c6r3s7.42seoul.kr>
2023-06-05 17:40:27 +0900

    :label: #2 Feat: set parser struct, enum

--- src/mandatory/includes/struct.h
+++ src/mandatory/includes/struct.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   struct.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
+/*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 11:08:43 by jgo               #+#    #+#             */
-/*   Updated: 2023/05/28 21:25:00 by jgo              ###   ########.fr       */
+/*   Updated: 2023/06/05 17:12:38 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,21 @@ typedef struct s_vec3		t_vec3;
 typedef struct s_vec3		t_point3;
 typedef struct s_vec3		t_color3;
 typedef struct s_ray		t_ray;
-typedef struct s_camera		t_camera;
+// typedef struct s_camera		t_camera;
 typedef struct s_canvas		t_canvas;
-typedef struct s_sphere		t_sphere;
+// typedef struct s_sphere		t_sphere;
 typedef struct s_hit_record	t_hit_record;
 typedef struct s_object		t_object;
-typedef struct s_light		t_light;
+// typedef struct s_light		t_light;
 typedef struct s_scene		t_scene;
+
+typedef struct s_rgb		t_rgb;
+typedef struct s_ambient	t_ambient;
+typedef struct s_camera		t_camera;
+typedef struct s_light		t_light;
+typedef struct s_sphere		t_sphere;
+typedef struct s_plane		t_plane;
+typedef struct s_cylinder	t_cylinder;
 
 typedef struct s_mlx		t_mlx;
 typedef struct s_img		t_img;
@@ -45,16 +53,16 @@ struct						s_ray
 	t_vec3					dir;
 };
 
-struct						s_camera
-{
-	t_point3 orig;        // 카메라 원점(위치)
-	double viewport_h;    // 뷰포트 세로길이
-	double viewport_w;    // 뷰포트 가로길이
-	t_vec3 horizontal;    // 수평길이 벡터
-	t_vec3 vertical;      // 수직길이 벡터
-	double focal_len;     // focal length
-	t_point3 left_bottom; // 왼쪽 아래 코너점
-};
+// struct						s_camera
+// {
+// 	t_point3 orig;        // 카메라 원점(위치)
+// 	double viewport_h;    // 뷰포트 세로길이
+// 	double viewport_w;    // 뷰포트 가로길이
+// 	t_vec3 horizontal;    // 수평길이 벡터
+// 	t_vec3 vertical;      // 수직길이 벡터
+// 	double focal_len;     // focal length
+// 	t_point3 left_bottom; // 왼쪽 아래 코너점
+// };
 
 struct						s_canvas
 {
@@ -63,12 +71,12 @@ struct						s_canvas
 	double aspect_ratio; //종횡비
 };
 
-struct						s_sphere
-{
-	t_point3				center;
-	double					radius;
-	double					radius2;
-};
+// struct						s_sphere
+// {
+// 	t_point3				center;
+// 	double					radius;
+// 	double					radius2;
+// };
 
 struct						s_hit_record
 {
@@ -126,6 +134,66 @@ struct						s_mlx
 struct						s_meta
 {
 	t_mlx					mlx;
+	t_ambient				ambient;
+	t_camera				camera;
+	t_light					light;
+	t_list					objs;
+};
+
+struct						s_rgb
+{
+	int						r;
+	int						g;
+	int						b;
+};
+
+struct						s_ambient
+{
+	t_object_type			type;
+	double					ratio;
+	t_rgb					rgb;
+};
+
+struct						s_camera
+{
+	t_object_type			type;
+	t_point3				view_point;
+	t_vec3					normal_vec3;
+	int						fov;
+};
+
+struct						s_light
+{
+	t_object_type			type;
+	t_point3				light_point;
+	double					ratio;
+	t_rgb					rgb;
+};
+
+struct						s_sphere
+{
+	t_object_type			type;
+	t_point3				center_point;
+	double					diameter;
+	t_rgb					rgb;
+};
+
+struct						s_plane
+{
+	t_object_type			type;
+	t_point3				point;
+	t_vec3					normal_vec3;
+	t_rgb					rgb;
+};
+
+struct						s_cylinder
+{
+	t_object_type			type;
+	t_point3				center_point;
+	t_vec3					normal_vec3;
+	double					diameter;
+	double					height;
+	t_rgb					rgb;
 };
 
 #endif
