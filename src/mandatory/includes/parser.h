0d61a7c7c46eae3089c7856dbea85f94035c571a
jgo-42 <jingmuio@gmail.com>
2023-06-07 17:10:10 +0900

    :fire: Chore(src): cleanup practice

--- src/mandatory/includes/parser.h
+++ src/mandatory/includes/parser.h
@@ -3,16 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 14:56:30 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/06/05 14:56:54 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/06/07 17:04:25 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
-
-
 
 #endif
--- src/mandatory/utils/hooks.c
+++ src/mandatory/utils/hooks.c
@@ -6,18 +6,19 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 17:08:41 by jgo               #+#    #+#             */
-/*   Updated: 2023/06/06 18:51:55 by jgo              ###   ########.fr       */
+/*   Updated: 2023/06/07 17:04:35 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minirt.h"
 #include "defs.h"
+#include "minirt.h"
 #include "utils.h"
 
 void	hooks(void *param)
 {
-	const t_meta *meta = param;
+	const t_meta	*meta;
 
+	meta = param;
 	if (mlx_is_key_down(meta->mlx_assets.mlx, MLX_KEY_ESCAPE))
 		destroy(meta);
 }
