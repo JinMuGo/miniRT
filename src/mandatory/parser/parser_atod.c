5805df3d7a4e54ef053364d151bbc47a7cbdc1f2
Sanghwa Lee <sanghwal@c3r8s7.42seoul.kr>
2023-06-07 19:35:54 +0900

    :truck: #1 Rename: atod()를 utils로 위치 변경

---
+++ src/mandatory/parser/parser_atod.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_atod.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/07 15:29:33 by sanghwal          #+#    #+#             */
+/*   Updated: 2023/06/07 19:21:37 by sanghwal         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "defs.h"
+#include "parser.h"
+#include "utils.h"
+
+double	check_to_double(char *str)
+{
+	int		len;
+	int		i;
+	int		dot_cnt;
+
+	len = ft_strlen(str);
+	i = -1;
+	dot_cnt = 0;
+	while (++i < len)
+	{
+		if (!ft_isdigit(str[i]) && ft_strncmp(&str[i], ".", 1))
+			parser_error("Invaild information");
+		if (!ft_strncmp(&str[i], ".", 1))
+			dot_cnt++;
+	}
+	if (dot_cnt > 1)
+		parser_error("Invaild information");
+	return(atod(str));
+}
