6f453009a2effb26cd06be6b402644760c52f63c
Sanghwa Lee <sanghwal@c3r8s7.42seoul.kr>
2023-06-08 14:11:46 +0900

    :sparkles: #13 Feat(parser): range 유효성 검사 함수

--- src/mandatory/includes/parser.h
+++ src/mandatory/includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 14:56:30 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/06/07 19:13:48 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/06/08 13:55:55 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ void	parser_error(char *str);
 char	**ft_split_whitespace(char const *s);
 t_rgb	parse_rgb(char *str);
 
-
 double	check_to_double(char *str);
+bool	check_minus1_to_1(double data);
+bool	check_rgb(t_rgb rgb);
+bool	check_0_to_1(double data);
 
 #endif
--- src/mandatory/parser/parser_utils.c
+++ src/mandatory/parser/parser_utils.c
@@ -6,9 +6,26 @@
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 15:29:33 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/06/07 19:14:37 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/06/08 14:09:06 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "defs.h"
 #include "parser.h"
+
+bool	check_0_to_1(double data)
+{
+	return (data >= 0.0 && data <= 1.0);
+}
+
+bool	check_rgb(t_rgb rgb)
+{
+	return ((rgb.r >= 0 && rgb.r <= 255) \
+		&& (rgb.g >= 0 && rgb.g <= 255) \
+		&& (rgb.b >= 0 && rgb.b <= 255));
+}
+
+bool	check_minus1_to_1(double data)
+{
+	return (data > -1.0 && data < 1.0);
+}
