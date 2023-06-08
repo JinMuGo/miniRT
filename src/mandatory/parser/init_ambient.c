22e032651ccb68cd35bbf094e7aef8f7ababe4b2
Sanghwa Lee <sanghwal@c3r8s7.42seoul.kr>
2023-06-08 13:36:06 +0900

    :sparkles: #1 Feat: init_ambient()

--- src/mandatory/parser/init_ambient.c
+++ src/mandatory/parser/init_ambient.c
@@ -6,7 +6,7 @@
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 17:55:28 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/06/07 18:51:01 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/06/08 13:33:20 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,29 @@
 #include "utils.h"
 #include "design_patterns.h"
 
-// void	init_ambient(char **line)
-// {
-// 	t_meta		*meta;
-// 	t_ambient	amb;
-// 	char		**temp;
+static void	vaildation_ambient(t_ambient amb)
+{
+	if (amb.type != AMB)
+		return (false);
+	if (!check_range_0_to_1(amb.ratio))
+		return (false);
+	if (!check_range_rgb(amb.rgb))
+		return (false);
+	return (true);
+}
 
-// 	if (ft_arrlen(line) != 3)
-// 		parser_error("Incorrect number of ambient information\n");
-// 	amb.type = AMB;
-// 	amb.ratio = check_to_double(line[1]);
-// 	// 0~255
-// 	temp = point3_split(line[2]);
+void	init_ambient(char **line)
+{
+	t_meta		*meta;
+	t_ambient	amb;
 
-// 	meta = singleton();
-// 	meta->ambient = amb;
-// }
+	if (ft_arrlen(line) != 3)
+		parser_error("Incorrect number of ambient information\n");
+	amb.type = AMB;
+	amb.ratio = check_to_double(line[1]);
+	amb.rgb = parse_rgb(line[2]);
+	if (!validation_ambient(amb))
+		parser_error("Invaild ambient\n");
+	meta = singleton();
+	meta->ambient = amb;
+}
