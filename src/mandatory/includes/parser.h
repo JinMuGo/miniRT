fdb3ebcd6a58198a482412bb5cf4855a33d9a847
Sanghwa Lee <sanghwal@c3r8s7.42seoul.kr>
2023-06-08 14:52:37 +0900

    :truck: #1 Rename: init_ambient 를 parse_ambient 로 변경

--- src/mandatory/includes/parser.h
+++ src/mandatory/includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 14:56:30 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/06/07 19:13:48 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/06/08 14:49:59 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 # define PARSER_H
 
 void	parser(char *file);
-void	parser_error(char *str);
 char	**ft_split_whitespace(char const *s);
+void	parse_ambient(char **line);
 t_rgb	parse_rgb(char *str);
 
 
 double	check_to_double(char *str);
 
+void	parser_error(char *str);
 #endif
