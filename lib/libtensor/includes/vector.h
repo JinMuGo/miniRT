3e2d4bdad10f74a397de026d16f811a6221007f8
jgo-42 <jingmuio@gmail.com>
2023-06-10 21:27:23 +0900

    :sparkles: Feat(lib): scalar_divide

--- lib/libtensor/includes/vector.h
+++ lib/libtensor/includes/vector.h
@@ -6,7 +6,7 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 13:53:14 by jgo               #+#    #+#             */
-/*   Updated: 2023/06/08 15:44:38 by jgo              ###   ########.fr       */
+/*   Updated: 2023/06/10 20:28:37 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ struct					s_vec4
 	double				w;
 };
 
+// oper
 t_vec3	vec3_init(double x, double y, double z);
 t_vec4	vec4_init(double x, double y, double z, double w);
 t_vec3	vec3_plus(t_vec3 a, t_vec3 b);
@@ -41,6 +42,10 @@ t_vec3	vec3_multi(t_vec3 a, t_vec3 b);
 t_vec3	vec3_cross_product(t_vec3 a, t_vec3 b);
 double	vec3_inner_product(t_vec3 a, t_vec3 b);
 
+// oper2
+t_vec3	vec3_scalar_divide(t_vec3 vec3, double scalar);
+
+// utils
 double	vec3_square_length(t_vec3 vec3);
 double	vec3_length(t_vec3 vec3);
 t_vec3	vec3_unit(t_vec3 vec3);
