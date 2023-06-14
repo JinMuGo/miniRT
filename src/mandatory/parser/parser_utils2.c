ecc69cef3adcea3e34d118ba5c536e96c72fdb79
idealflower-k <aekrcu@naver.com>
2023-06-14 16:19:48 +0900

    :recycle: Refactor(parser) : Improve accuracy vaildation_normal_vec3
    #1 법선벡터 정확도 향상 use EPSILON

--- src/mandatory/parser/parser_utils2.c
+++ src/mandatory/parser/parser_utils2.c
@@ -6,7 +6,7 @@
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 14:47:06 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/06/14 15:51:59 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/06/14 16:17:52 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ bool	check_normal_vec(t_vec3 vector)
 	if (!check_minus1_to_1(vector.x)
 		|| !check_minus1_to_1(vector.y)
 		|| !check_minus1_to_1(vector.z)
-		|| (vector.x == 0 && vector.y == 0 && vector.z == 0))
+		|| (fabs(vector.x - 0.0) < EPSILON
+			&& fabs(vector.y - 0.0) < EPSILON
+			&& fabs(vector.z - 0.0) < EPSILON))
 		return (false);
 	return (true);
 }
