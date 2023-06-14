dddf10c1ed7ef0926a6234b04f7bd84038ab4c5d
idealflower-k <aekrcu@naver.com>
2023-06-14 16:59:02 +0900

    :recycle: Refactor(parser): delete EPSILON

--- src/mandatory/parser/parser_utils2.c
+++ src/mandatory/parser/parser_utils2.c
@@ -6,7 +6,7 @@
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 14:47:06 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/06/14 16:17:52 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/06/14 16:57:23 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ bool	check_normal_vec(t_vec3 vector)
 	if (!check_minus1_to_1(vector.x)
 		|| !check_minus1_to_1(vector.y)
 		|| !check_minus1_to_1(vector.z)
-		|| (fabs(vector.x - 0.0) < EPSILON
-			&& fabs(vector.y - 0.0) < EPSILON
-			&& fabs(vector.z - 0.0) < EPSILON))
+		|| (vector.x == 0
+			&& vector.y == 0
+			&& vector.z == 0))
 		return (false);
 	return (true);
 }
