7098003a555de901b22b9237524889ce9987ee55
jgo-42 <jingmuio@gmail.com>
2023-06-14 14:07:45 +0900

    :fire: Remove: draw_background

--- src/mandatory/scene/setup_scene.c
+++ src/mandatory/scene/setup_scene.c
@@ -6,7 +6,7 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 12:25:54 by jgo               #+#    #+#             */
-/*   Updated: 2023/06/13 17:25:10 by jgo              ###   ########.fr       */
+/*   Updated: 2023/06/14 14:06:44 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,28 +39,8 @@ static inline void	_setup_camera(t_camera *camera, double aspect_ratio)
 	camera->left_bottom = vec3_minus(camera->pos, vec3_plus(vec3_plus(vec3_scalar_multi(camera->x, camera->viewport_w / 2), vec3_scalar_multi(camera->y, camera->viewport_h / 2)), camera->z));
 }
 
-static inline void	_render_background(t_meta *meta)
-{
-	const t_rgba	rgba_bgr = rgba_init(42, 42, 42, 255);
-	int				axis[2];
-
-	axis[Y] = 0;
-	while (axis[Y] < WIN_HEIGHT)
-	{
-		axis[X] = 0;
-		while (axis[X] < WIN_WIDTH)
-		{
-			mlx_put_pixel(meta->mlx_assets.img, axis[X], axis[Y],
-				rgba_to_color(rgba_bgr));
-			axis[X]++;
-		}
-		axis[Y]++;
-	}
-}
-
 void	setup_scene(t_meta *meta)
 {
 	_setup_canvas(&meta->scene.canvas);
 	_setup_camera(&meta->camera, meta->scene.canvas.aspect_ratio);
-	_render_background(meta);
 }
