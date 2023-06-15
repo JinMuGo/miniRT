1c5c88f3300e4266f09ee44e4abea9a14dda804f
idealflower-k <aekrcu@naver.com>
2023-06-15 13:25:44 +0900

    :recycle: Style(libquadratic): delete ';' && add () in 2 * a
    중복 세미콜론 제거 및 (2 * a) 로 변경

--- lib/libtensor/libequation/quadratic.c
+++ lib/libtensor/libequation/quadratic.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   quadratic.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
+/*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 18:33:45 by jgo               #+#    #+#             */
-/*   Updated: 2023/06/12 18:51:29 by jgo              ###   ########.fr       */
+/*   Updated: 2023/06/15 13:24:08 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 bool	root_formula(double a, double b, double c, double root[2])
 {
-	const double	d = b * b - 4 * a * c;;
+	const double	d = b * b - 4 * a * c;
 
 	if (d < 0 || fabs(a) < EPSILON)
 		return (false);
-	root[0] = (-b - sqrt(d)) / 2 * a;
-	root[1] = (-b + sqrt(d)) / 2 * a;
+	root[0] = (-b - sqrt(d)) / (2 * a);
+	root[1] = (-b + sqrt(d)) / (2 * a);
 	return (true);
 }
