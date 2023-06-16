07befc668e47878d899055a51ad550b8c13d71c6
jgo-42 <jingmuio@gmail.com>
2023-06-16 18:12:47 +0900

    :sparkles: Feat(hooks); add hooks
    hooks deploy

--- src/mandatory/hooks/hooks.c
+++ src/mandatory/hooks/hooks.c
@@ -6,19 +6,36 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 17:08:41 by jgo               #+#    #+#             */
-/*   Updated: 2023/06/12 19:28:22 by jgo              ###   ########.fr       */
+/*   Updated: 2023/06/16 16:38:42 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "defs.h"
 #include "minirt.h"
 #include "utils.h"
+#include "scene.h"
+#include "render.h"
 
-void	hooks(void *param)
+void	key_hooks(mlx_key_data_t keydata, void *param)
 {
 	t_meta	*meta;
 
 	meta = param;
-	if (mlx_is_key_down(meta->mlx_assets.mlx, MLX_KEY_ESCAPE))
+	if (keydata.key == MLX_KEY_ESCAPE)
 		destroy(meta);
+}
+
+void	resize_hook(int32_t width, int32_t height, void *param)
+{
+	t_meta *meta;
+
+	meta = param;
+	setup_scene(meta, width, height);
+	render(meta);
+}
+
+void	hooks(t_meta *meta)
+{
+	mlx_resize_hook(meta->mlx_assets.mlx, resize_hook, meta);
+	mlx_key_hook(meta->mlx_assets.mlx, key_hooks, meta);
 }
