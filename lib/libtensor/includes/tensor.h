9f25e5d9b653ef58f0cd734143c8bde8cf50e149
jgo-42 <jingmuio@gmail.com>
2023-06-17 13:16:48 +0900

    :rocket: Draft: libmatrix deploy

--- lib/libtensor/includes/tensor.h
+++ lib/libtensor/includes/tensor.h
@@ -6,20 +6,22 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 18:49:21 by jgo               #+#    #+#             */
-/*   Updated: 2023/06/12 18:51:40 by jgo              ###   ########.fr       */
+/*   Updated: 2023/06/17 12:55:37 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TENSOR_H
-#define TENSOR_H
+# define TENSOR_H
 
-#include <math.h>
-#include <stdbool.h>
+# include <math.h>
+# include <stdbool.h>
 
 # define EPSILON 1e-6
 
 typedef struct s_vec3	t_vec3;
 typedef struct s_vec4	t_vec4;
+typedef struct s_mat4	t_mat4;
+typedef struct s_mat3	t_mat3;
 
 struct					s_vec3
 {
@@ -36,27 +38,53 @@ struct					s_vec4
 	double				w;
 };
 
+struct					s_mat4
+{
+	t_vec4				a;
+	t_vec4				b;
+	t_vec4				c;
+	t_vec4				d;
+};
+
+struct					s_mat3
+{
+	t_vec3				a;
+	t_vec3				b;
+	t_vec3				c;
+};
+
 // oper
-t_vec3	vec3_init(double x, double y, double z);
-t_vec4	vec4_init(double x, double y, double z, double w);
-t_vec3	vec3_plus(t_vec3 a, t_vec3 b);
-t_vec3	vec3_minus(t_vec3 a, t_vec3 b);
-t_vec3	vec3_multi(t_vec3 a, t_vec3 b);
-t_vec3	vec3_cross_product(t_vec3 a, t_vec3 b);
-double	vec3_inner_product(t_vec3 a, t_vec3 b);
+t_vec3					vec3_init(double x, double y, double z);
+t_vec4					vec4_init(double x, double y, double z, double w);
+t_vec3					vec3_plus(t_vec3 a, t_vec3 b);
+t_vec3					vec3_minus(t_vec3 a, t_vec3 b);
+t_vec3					vec3_multi(t_vec3 a, t_vec3 b);
+t_vec3					vec3_cross_product(t_vec3 a, t_vec3 b);
+double					vec3_inner_product(t_vec3 a, t_vec3 b);
 
 // oper2
-t_vec3	vec3_scalar_divide(t_vec3 vec3, double scalar);
-t_vec3	vec3_scalar_multi(t_vec3 vec3, double scalar);
+t_vec3					vec3_scalar_divide(t_vec3 vec3, double scalar);
+t_vec3					vec3_scalar_multi(t_vec3 vec3, double scalar);
 
 // utils
-double	vec3_square_length(t_vec3 vec3);
-double	vec3_length(t_vec3 vec3);
-t_vec3	vec3_unit(t_vec3 vec3);
+double					vec3_square_length(t_vec3 vec3);
+double					vec3_length(t_vec3 vec3);
+t_vec3					vec3_unit(t_vec3 vec3);
 
+double					degree_to_radian(double degree);
 
-double	degree_to_radian(double degree);
+bool					root_formula(double a, double b, double c,
+							double root[2]);
 
-bool	root_formula(double a, double b, double c, double root[2]);
+// vec4_oper
+
+t_vec4					vec4_plus(t_vec4 a, t_vec4 b);
+t_vec4					vec4_scalar_multi(t_vec4 vec4, double scalar);
+
+// mat
+t_mat4					mat4_init(t_vec4 a, t_vec4 b, t_vec4 c, t_vec4 d);
+t_mat4					mat4_init_one_val(double val);
+t_mat3					mat3_init(t_vec3 a, t_vec3 b, t_vec3 c);
+t_mat3					mat3_init_one_val(double val);
 
 #endif
