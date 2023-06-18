b71d1297a7b328962f56a3e7b27fafc65c83515e
jgo-42 <jingmuio@gmail.com>
2023-06-18 18:02:15 +0900

    :recycle: Refactor(libtensor): divide to multiply
    자주 사용되는 나누기 연산들 곱셈연산으로 변환.

--- lib/libtensor/libvector/vec3_oper2.c
+++ lib/libtensor/libvector/vec3_oper2.c
@@ -6,7 +6,7 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 20:22:04 by jgo               #+#    #+#             */
-/*   Updated: 2023/06/18 17:37:32 by jgo              ###   ########.fr       */
+/*   Updated: 2023/06/18 17:57:13 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 t_vec3	vec3_scalar_divide(t_vec3 vec3, double scalar)
 {
-	return (vec3_init(vec3.x / scalar, vec3.y / scalar, vec3.z / scalar));
+	return (vec3_init(
+			vec3.x * (1.0 / scalar),
+			vec3.y * (1.0 / scalar),
+			vec3.z * (1.0 / scalar)));
 }
 
 t_vec3	vec3_scalar_multi(t_vec3 vec3, double scalar)
