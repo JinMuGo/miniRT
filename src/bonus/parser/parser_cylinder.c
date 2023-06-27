ac944d2e6a7afc1f8004bee47fc0352877e0ddfc
JinMu Go <jingmuio@gmail.com>
2023-06-27 22:47:07 +0900

    :bento: Chore(cylinder_bump): cylinder parse bump

--- src/bonus/parser/parser_cylinder.c
+++ src/bonus/parser/parser_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 16:50:54 by sanghwal          #+#    #+#             */
-/*   Updated: 2023/06/22 16:16:38 by jgo              ###   ########.fr       */
+/*   Updated: 2023/06/27 22:46:17 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,38 +26,14 @@ static bool	vaildation_cylinder(t_cylinder *cylinder)
 	return (true);
 }
 
-static inline t_obj_option *_cb_allocator(char **line)
-{
-	t_obj_option *option;
-
-	option = ft_malloc(sizeof(t_obj_option));
-	option->type = CB;
-	option->op.cb.rgba = parser_rgba(line[7]);
-	option->op.cb.scale_s = check_to_double(line[8]);
-	option->op.cb.scale_t = check_to_double(line[9]);
-	option->op.cb.degree = check_to_double(line[10]);
-	return (option);
-}
-
-static inline t_obj_option	*_option_allocator(char **line)
-{
-	if (!ft_strcmp(line[6], "cb"))
-		return (_cb_allocator(line));
-	else
-		return (NULL);
-	// else if (!ft_strcmp(line[4], "bp"))
-	// 	cylindeer->option = _bp_allocator(line);
-}
-
 void	parser_cylinder(char **line)
 {
 	const int		len = ft_arrlen((void **)line);
 	t_obj_option	*option;
 	t_cylinder		cylinder;
-	t_meta			*meta;
 	t_obj			*obj;
 
-	if (!(len == 6 || len == 11))
+	if (!(len == 6 || len == 8 || len == 9 || len == 11))
 		error_handler(CY_ERR);
 	cylinder.type = CY;
 	cylinder.center_point = parser_point3(line[1]);
@@ -66,7 +42,7 @@ void	parser_cylinder(char **line)
 	cylinder.height = check_to_double(line[4]);
 	cylinder.rgba = parser_rgba(line[5]);
 	if (line[6])
-		option = _option_allocator(line);
+		option = option_allocator(line, 6, CY_ERR);
 	else
 		option = NULL;
 	if (!vaildation_cylinder(&cylinder) || !vaildation_option(option))
@@ -74,11 +50,10 @@ void	parser_cylinder(char **line)
 		ft_free_all_arr(line);
 		error_handler(CY_ERR);
 	}
-	meta = singleton();
 	obj = ft_malloc(sizeof(t_obj));
 	obj->type = CY;
 	obj->content.cylinder = cylinder;
 	obj->option = option;
 	obj->next = NULL;
-	objsadd_back(&meta->objs, obj);
+	objsadd_back(&singleton()->objs, obj);
 }
