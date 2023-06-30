6c259a8c4cfd7a959f224e5ed66777f85311ee1b
jgo-42 <jingmuio@gmail.com>
2023-06-30 19:27:36 +0900

    :pencil2: Chore(norm): norm lib files

--- lib/libtensor/libvector/vec3_utils2.c
+++ lib/libtensor/libvector/vec3_utils2.c
@@ -6,7 +6,7 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/25 21:47:30 by jgo               #+#    #+#             */
-/*   Updated: 2023/06/28 18:01:43 by jgo              ###   ########.fr       */
+/*   Updated: 2023/06/30 19:27:08 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ static inline t_vec3	_judge_up_vector(t_vec3 vec)
 
 void	set_ab_axis_from_c(t_vec3 *a, t_vec3 *b, const t_vec3 *c)
 {
-	const t_vec3 up = _judge_up_vector(*c);
+	const t_vec3	up = _judge_up_vector(*c);
 
 	*a = vec3_unit(vec3_cross_product(up, *c));
 	*b = vec3_unit(vec3_cross_product(*c, *a));
